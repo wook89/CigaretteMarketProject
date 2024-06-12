12737bd5d1aaa9bf98462565b19d0dcda1a0f349
wook89 <162427559+wook89@users.noreply.github.com>
2024-06-12 22:33:51 +0900

    Update CigarMarket.java

--- src/CigarMarket/CigarMarket.java
+++ src/CigarMarket/CigarMarket.java
@@ -1,10 +1,23 @@
 package CigarMarket;
 
+import java.io.IOException;
+
+import CigarMarket.controller.CigarMarketController;
+import CigarMarket.model.Cart;
+import CigarMarket.model.CigarStorage;
+import CigarMarket.view.ConsoleView;
+
 public class CigarMarket {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args)throws IOException {
+		//model 
+		CigarStorage cigarStorage = new CigarStorage();
+		Cart cart = new Cart();
+		//view
+		ConsoleView view = new ConsoleView();
+		//controller
+		CigarMarketController controller = new CigarMarketController(cigarStorage,cart,view);
+		controller.start();
 	}
 
 }

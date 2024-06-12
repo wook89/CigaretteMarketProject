e488c6acd4ad15133f8d8fbcc7297a1c4362e8de
wook89 <162427559+wook89@users.noreply.github.com>
2024-06-12 22:34:38 +0900

    Update Admin.java

--- src/CigarMarket/model/Admin.java
+++ src/CigarMarket/model/Admin.java
@@ -1,5 +1,12 @@
 package CigarMarket.model;
 
 public class Admin {
+	private String id = "admin";
+	private String pw = "1234";
+	public boolean login(String id, String pw) {
+		if(this.id.equals(id) && this.pw.equals(pw))
+			return true;
+		return false;
+	}
 
 }

39a23ad1557eda43b5e057b38c5b5ba99dafac9a
wook89 <162427559+wook89@users.noreply.github.com>
2024-06-12 22:35:17 +0900

    Update CartItem.java

--- src/CigarMarket/model/CartItem.java
+++ src/CigarMarket/model/CartItem.java
@@ -1,5 +1,51 @@
 package CigarMarket.model;
 
 public class CartItem {
+    
+    Cigar cigar;
+    int cigarId;
+    int quantity;
+    
+    public CartItem(Cigar cigar) {
+        this.cigar = cigar;
+        this.cigarId = cigar.getCigarId();
+        this.quantity = 1;
+    }
+    
+    public Cigar getCigar() {
+        return cigar;
+    }
 
+    public void setCigar(Cigar cigar) {
+        this.cigar = cigar;
+    }
+
+    public int getCigarId() {
+        return cigarId;
+    }
+
+    public void setCigarId(int cigarId) {
+        this.cigarId = cigarId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getPrice() {
+        return quantity * cigar.getPrice();
+    }
+
+    public void addQuantity(int quantity) {
+        this.quantity += quantity;
+    }
+    
+    @Override
+    public String toString() {
+        return cigar.getCigarId() + " <" + cigar.getName() + "> [" + quantity + "개] " + getPrice() + "원";
+    }
 }

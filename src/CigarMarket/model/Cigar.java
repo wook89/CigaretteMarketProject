ec86a11580f62ee346846c66c419d1407a5c289c
wook89 <162427559+wook89@users.noreply.github.com>
2024-06-12 22:35:39 +0900

    Update Cigar.java

--- src/CigarMarket/model/Cigar.java
+++ src/CigarMarket/model/Cigar.java
@@ -1,5 +1,63 @@
 package CigarMarket.model;
 
 public class Cigar {
+	private int cigarId;
+	private String name;
+	private String tar;
+	private String nicotine;
+	private int price;
+	
+	public Cigar(int cigarId, String name, String tar, String nicotine, int price) {
+		this.cigarId = cigarId;
+		this.name = name;
+		this.tar = tar;
+		this.nicotine = nicotine;
+		this.price = price;	
+	}
+	
+	public int getCigarId() {
+		return cigarId;
+	}
 
+	public void setCigarId(int cigarId) {
+		this.cigarId = cigarId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getTar() {
+		return tar;
+	}
+
+	public void setTar(String tar) {
+		this.tar = tar;
+	}
+
+	public String getNicotine() {
+		return nicotine;
+	}
+
+	public void setNicotine(String nicotine) {
+		this.nicotine = nicotine;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	@Override
+	public String toString() {
+		return cigarId+"  <"+name+"> "+tar+", "+nicotine+"  ["+price+"원]";
+	}
+	
 }
